b65bc5a1e0016c78834e3890363133574e7d42a8
Marco Ordoñez <marco@evolbit.net>
2023-08-24 18:37:08 +0200

    Fix regex for larger domains

--- components/home/signup-pre-launch.tsx
+++ components/home/signup-pre-launch.tsx
@@ -12,7 +12,7 @@ const SignUpPreLaunch = () => {
 		let error;
 		if (!value) {
 			error = "Enter an email address";
-		} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+		} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,8}$/i.test(value)) {
 			error = "Invalid email address";
 		}
 		return error;
